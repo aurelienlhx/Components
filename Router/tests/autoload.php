4057cbb83010bc6a97efec132f35ce05c1f17ac9
aurelienlhx <dev@aurelienlheureux.com>
2016-02-16 21:33:19 +0100

    Remove Router

--- Router/tests/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-spl_autoload_register(function($class_name) {
-	//basename use for namespace notation
-	$path = realpath(dirname(__DIR__).'/src/' . basename($class_name) . '.php');
-    if(is_file($path))
-    	require_once($path);
-});
